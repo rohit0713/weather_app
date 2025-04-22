271355c82a3dbc799ddcb6bef39a53104feb369a
Rohit Pandey <rohit.9936230612@gmail.com>
2025-04-22 16:35:14 +0530

    Repo Updated

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,8 +1,8 @@
 import 'package:get_it/get_it.dart';
 import 'package:weather_app/core/network/dio_network_service.dart';
 import 'package:weather_app/core/network/network_service.dart';
-import 'package:weather_app/features/weather/repo/weather_repo.dart';
-import 'package:weather_app/features/weather/services/weather_service.dart';
+import 'package:weather_app/features/weather/data_source/weather_repo.dart';
+import 'package:weather_app/features/weather/data_source/weather_service.dart';
 
 final serviceLocator = GetIt.instance;
 
