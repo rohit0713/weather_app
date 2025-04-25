bb0f7f4bccbc06d8c8aa461a83ac1e743ddd659f
rohit0713 <135029804+rohit0713@users.noreply.github.com>
2025-04-25 15:32:28 +0530

    Update api_key.dart

--- lib/constants/api_key.dart
+++ lib/constants/api_key.dart
@@ -1,5 +1,5 @@
 class ApiKey {
-  static String get WEATHER_API_KEY=>'f7d916fa5cdd9ede79dffb614aa13ed5';
+  static String get WEATHER_API_KEY=>'Your Weather API KEY';
 
-   static String get GOOGLE_API_KEY=>'AIzaSyCraai9uNsRH93WwJkcvX2sYmgZFqK1KUs';
+   static String get GOOGLE_API_KEY=>'Your Google API KEY';
 }
